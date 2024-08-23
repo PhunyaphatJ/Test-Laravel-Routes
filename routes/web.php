07b8fb88cf1d63a58f1e91c91e1cba9f12c3b7f3
Phunyaphat <phunyaphatjamja@gmail.com>
2024-08-24 01:33:53 +0700

    update web route

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::middleware(['auth'])->group(function(){
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        Route::resource('/tasks',TaskController::class);
+        Route::resource('tasks',TaskController::class);
 
     // End of the /app Route Group
     });
@@ -87,7 +87,7 @@ Route::middleware(['auth'])->group(function(){
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('/stats',[AdminDashboard::class,'Single Action']);
+        Route::get('/stats',AdminDashboard::class);
 
     // End of the /admin Route Group
     });
